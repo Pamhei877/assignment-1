d11cb49d5bf522d8d65ad2cd634767c815618262
Pamhei877 <154424407+Pamhei877@users.noreply.github.com>
2023-12-21 12:42:04 +0530

    Create product.c

---
+++ product.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+void main()
+
+{
+
+int a,b,c;
+
+printf(“Enter the two numbers :”);
+
+scanf(“%d%d”,&a,&b);
+
+c=a*b;
+
+printf(“Multiplication: %d”,c);
+
+getch(); //if you are writing in windows
+
+}
+
+2. Provide default value
+
+void main ()
+
+{
+
+int a=10,b=20,c;
+
+c=a*b;
+
+printf(“Multiplication: %d”,c);
+
+getch(); //if you are writing in windows
+
+}
